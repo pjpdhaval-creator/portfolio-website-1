603c8fdd66bfbd6a0464c0a280fef9c3e1474e15
pjpdhaval-creator <pjpdhaval@gmail.com>
2026-04-28 10:53:12 +0530

    Switch to portfolio-website-1 and deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/portfolio-website/' : '/',
+  base: mode === 'production' ? '/portfolio-website-1/' : '/',
   plugins: [
     react(),
     tailwindcss(),
